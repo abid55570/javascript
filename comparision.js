a4126c6652f33af312d848389b08060a06130375
abid55570 <124482868+abid55570@users.noreply.github.com>
2024-01-16 11:29:45 +0530

    primitive non premitive

--- comparision.js
+++ comparision.js
@@ -20,4 +20,36 @@
 
 // ===
 
-console.log("2"===2);
+// console.log("2"===2);
+
+// **** Data Type ****//
+
+// Primitive data type & Non Primitive(or by Reference)
+// Primitive ==>
+// 7 Types : String, Number, boolean, null, Undefined, Symbol,BigInt
+
+
+const score = 100 
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const otherID = Symbol('123')
+
+//console.log(id===otherID); // not equal because symbol provide uniquesness
+
+const bigNumber = 587345736573560n
+
+// Non Primitive (Reference) : Array, Objects, Functions
+
+const heros = ["kala", "aladin", "shaktiman", "juniorG"]
+let myObj = {
+    namee: "abcd",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Good Morning");
+}
