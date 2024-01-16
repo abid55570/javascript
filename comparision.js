28f61d5d0db2518ebbc7667a555d6d93a742780f
abid55570 <124482868+abid55570@users.noreply.github.com>
2024-01-16 11:42:48 +0530

    memory allocation

--- comparision.js
+++ comparision.js
@@ -29,27 +29,48 @@
 // 7 Types : String, Number, boolean, null, Undefined, Symbol,BigInt
 
 
-const score = 100 
-const scoreValue = 100.3
-const isLoggedIn = false
-const outsideTemp = null
-let userEmail;
+// const score = 100 
+// const scoreValue = 100.3
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
 
-const id = Symbol('123')
-const otherID = Symbol('123')
+// const id = Symbol('123')
+// const otherID = Symbol('123')
 
-//console.log(id===otherID); // not equal because symbol provide uniquesness
+// //console.log(id===otherID); // not equal because symbol provide uniquesness
 
-const bigNumber = 587345736573560n
+// const bigNumber = 587345736573560n
 
-// Non Primitive (Reference) : Array, Objects, Functions
+// // Non Primitive (Reference) : Array, Objects, Functions
 
-const heros = ["kala", "aladin", "shaktiman", "juniorG"]
-let myObj = {
-    namee: "abcd",
-    age: 22,
+// const heros = ["shaktiman", "juniorG"]
+// let myObj = {
+//     namee: "abcd",
+//     age: 22,
+// }
+
+// const myFunction = function(){
+//     console.log("Good Morning");
+// }
+// ******************************************************//
+
+// stack memory => Primitive type use this memory : provides a copy  
+// heap memory => non primitive : provide oroginal value or chanes in original values
+let name1 = "abcd1234"
+let name2 = name1
+name2 = "mycode"
+
+console.log(name1);
+console.log(name2);
+
+let user1 = {
+    email: "user@google.com",
+    upi: "user@ybl"
 }
+let user2 = user1
 
-const myFunction = function(){
-    console.log("Good Morning");
-}
+user2.email = "abid@google.com"
+
+console.log(user1);
+console.log(user2);
