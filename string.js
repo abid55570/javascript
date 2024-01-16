b2df928f224a75ba7fdff2235f3fd03925306a65
abid55570 <124482868+abid55570@users.noreply.github.com>
2024-01-16 12:19:22 +0530

    strings like python

---
+++ string.js
@@ -0,0 +1,27 @@
+const name = "abid"
+const repoCount = 50
+// console.log(name + repoCount + "value");
+
+// we will not use this
+// we use f string like python as eg. 
+
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('abcd')
+
+console.log(gameName[3]); // it is like slicing of python 
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('c'));
+
+const newString = gameName.substring(0,3)
+console.log(newString);
+
+// same as python slicing works 
+
+const url = "https://google.com/home%20work"
+// url.replace('%20','-')
+// console.log(url.replace('%20', '-'));
+console.log(url.includes('%20'));
