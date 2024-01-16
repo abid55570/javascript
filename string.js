b53dd3ec764f35df7fefb3c54bff05273bee8161
abid55570 <124482868+abid55570@users.noreply.github.com>
2024-01-16 12:41:59 +0530

    maths

--- string.js
+++ string.js
@@ -5,24 +5,40 @@ const repoCount = 50
 // we will not use this
 // we use f string like python as eg. 
 
-console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+// console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('abcd')
+// const gameName = new String('abcd')
 
-console.log(gameName[3]); // it is like slicing of python 
+// console.log(gameName[3]); // it is like slicing of python 
 
-console.log(gameName.length);
-console.log(gameName.toUpperCase());
-console.log(gameName.charAt(2));
-console.log(gameName.indexOf('c'));
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.indexOf('c'));
+// console.log(gameName.charAt(2));
 
-const newString = gameName.substring(0,3)
-console.log(newString);
+// const newString = gameName.substring(0,3)
+// console.log(newString);
 
 // same as python slicing works 
 
 const url = "https://google.com/home%20work"
 // url.replace('%20','-')
 // console.log(url.replace('%20', '-'));
-console.log(url.includes('%20')); //shows the elements present in the url or not output will be in boolean 
+// console.log(url.includes('%20')); //shows the elements present in the url or not output will be in boolean 
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.floor(4.9));
+
+// console.log(Math.random());
+// console.log(Math.floor(Math.random()*10) +1);
+
+const min =10
+const max =20
+
+console.log(Math.floor(Math.random() * (max-min +1)) + min)
+
+
+
 
