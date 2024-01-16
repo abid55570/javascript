d4de265aff86f8fbda7a41743ce761fc92e11541
abid55570 <124482868+abid55570@users.noreply.github.com>
2024-01-16 18:35:55 +0530

    concat & push

--- basic_2/Arrays.js
+++ basic_2/Arrays.js
@@ -18,11 +18,25 @@ const myArray = [1,1,2,3,4, "abcd"]
 
 //slice splice ********************************
 
-const myn1 = myArray.slice(1,3) //1,3 is range of elements
-console.log(myn1)
-console.log("B", myArray);
+// const myn1 = myArray.slice(1,3) //1,3 is range of elements
+// console.log(myn1)
+// console.log("B", myArray);
 
-const myn2 = myArray.splice(1,3) // 1,3 last range print 
-console.log("C", myArray);
-console.log(myn2);
+// const myn2 = myArray.splice(1,3) // 1,3 last range print 
+// console.log("C", myArray);
+// console.log(myn2);
 // in splice last range include and in slice last range not taken
+
+// ************** //*****************//
+
+const marvel_heros = ["thor","ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+marvel_heros.push(dc_heros)
+console.log(marvel_heros);
+// it works like python adding list in list with "PUSH" 
+// same we can do with the "concat" lets try this one too 
+
+marvel_heros.concat(dc_heros)
+console.log(marvel_heros);
+// result is same 
