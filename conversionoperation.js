c459cb5ef55a26d50758ad5aa9938be3c94710a8
abid55570 <124482868+abid55570@users.noreply.github.com>
2024-01-16 10:53:45 +0530

    conversions

---
+++ conversionoperation.js
@@ -0,0 +1,32 @@
+// let score = "33" 
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof score);
+// console.log(typeof valueInNumber);
+
+// let score = "null" 
+// // console.log(typeof score);
+// // console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof score);
+// console.log(typeof valueInNumber);
+
+// let score = "22bfg" 
+// // console.log(typeof score);
+// // console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// // console.log(typeof score);
+// console.log(typeof valueInNumber);
+
+// "33" => 33
+// "33khg"=> NaN
+// True => 1; False => 0 
+
+
+let isLoggedIn =1 
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
