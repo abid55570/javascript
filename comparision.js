82ddf62e365bb30ba247111c74645ee2ed6b3e76
abid55570 <124482868+abid55570@users.noreply.github.com>
2024-01-16 11:12:48 +0530

    comparision

---
+++ comparision.js
@@ -0,0 +1,23 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2 !=1);
+
+// console.log("2">1);
+// console.log("02">1);
+// typescript rule 
+// ***********Avoid below conversion*******
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+
+// console.log(undefined==0);
+
+// console.log(undefined>0);
+
+// console.log(undefined<=0);
+
+// ===
+
+console.log("2"===2);
