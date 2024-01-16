9df416ced988a071553c27650c2e3847af6ea570
abid55570 <124482868+abid55570@users.noreply.github.com>
2024-01-16 13:34:33 +0530

    localestring

--- dates.js
+++ dates.js
@@ -1,12 +1,26 @@
-let myDate = new Date()
-console.log(myDate);
+// let myDate = new Date()
+// console.log(myDate);
 
 // let myDate = new Date()
-console.log(myDate.toString());
+// console.log(myDate.toString());
 
 // let myDate = new Date()
-console.log(myDate.toLocaleString());
-console.log(typeof myDate);
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
 
-let myCreatedDate = new Date(2024,8,9)
-console.log(myCreatedDate.toDateString());
+// let myCreatedDate = new Date(2024,8,9)
+// console.log(myCreatedDate.toDateString());
+
+// let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() +1);
+console.log(newDate.getDay());
+
+newDate.toLocaleString('default', {
+    weekday: "long",
+    year: "2-digit"
+})
+console.log(newDate.toLocaleString());
