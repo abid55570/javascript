ff932c1e08faf6f060d23bb689c813d0b5f271e3
abid55570 <124482868+abid55570@users.noreply.github.com>
2024-01-16 13:16:24 +0530

    date

---
+++ dates.js
@@ -0,0 +1,12 @@
+let myDate = new Date()
+console.log(myDate);
+
+// let myDate = new Date()
+console.log(myDate.toString());
+
+// let myDate = new Date()
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
+
+let myCreatedDate = new Date(2024,8,9)
+console.log(myCreatedDate.toDateString());
