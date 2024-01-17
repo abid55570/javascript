4f341f4abc7251c340656cb5f93408f4d7a90b1f
abid55570 <124482868+abid55570@users.noreply.github.com>
2024-01-17 19:23:29 +0530

    nullish terniary

---
+++ basic_3/if_else.js
@@ -0,0 +1,68 @@
+
+// const temperature  =51
+
+// if (temperature<50){
+//     console.log("less than 50");
+//     // process if condition is true
+// } else {
+//     console.log("temp is greater than 50");
+// }
+// if (false){
+//     // do not process from here if condition is false
+// }
+
+// const userLoggedin = true
+// const debitCard = true
+// const loggedinFromGoogle = false
+// const loggedinfromemail = true
+
+// if (userLoggedin && debitCard && 2==2){
+//     console.log("allow to buy course");
+// }
+
+// if (loggedinFromGoogle || loggedinfromemail) {
+//     console.log("user logged in");
+// }
+
+// const month = "march"
+
+// switch (month) {
+//     case 1:
+//         console.log("january");
+//         break;
+//     case 2:
+//         console.log("febuary");
+//         break;
+//     case "march":
+//         console.log("march");
+//         break;
+//     case "april":
+//         console.log("april");
+//         break;
+
+//     default:
+//         console.log("value not match");
+//         break;
+// }
+
+
+// >>>> Nullish coalescing Operator ?? null undefined
+
+// let val1;
+// // val1= 5 ?? 10 
+// // val1= null ?? 10 
+// // val1= undefined ?? 10 
+// // val1=null?? 5 ?? 10 
+// val1=null?? 5 ?? function(){}
+
+//  this is use for exception programme such as data not received from database or provide null or undefined value then next function will be executed
+
+// console.log(val1);
+
+
+// terniary operator >>>>>>>>>>>>>>>>>>>>>.
+
+// condition ? true : false
+
+const iceteaprice = 100 
+iceteaprice <= 80? console.log("less than 80") : console.log("more than 80");
