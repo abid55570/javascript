2822b8581a89e10251a87737dab8a7a5fb2d0190
abid55570 <124482868+abid55570@users.noreply.github.com>
2024-01-17 19:59:25 +0530

    loop

---
+++ basic_3/iteration.js
@@ -0,0 +1,37 @@
+// for 
+
+// for (let index = 0; index < 10; index++) {
+//     const element = index;
+//     console.log(element);
+// }
+// for (let index = 0; index < 10; index++) {
+//     const element = index;
+//     if (element==5){
+//         console.log("5 is the number");
+//     }
+//     console.log(element);
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`outer loop value : ${i}`);
+//     for (let j = 1; j <= 10; j++) {
+//         console.log(i+ '*' + j + ' = ' + i*j);     
+//     }
+// }
+
+// let index = 0 
+// while (index<=10){
+//     console.log(`value of index is ${index}`);
+//     index= index+2
+// }
+
+
+const arr = [1,2,3,4,5]
+
+// for (const num of arr) {
+//     console.log(num);
+// }
+
+// map >>>>>>>>>>>>>>>>>>>>>>>
+
+// foreach >>> fetching the data from databases
